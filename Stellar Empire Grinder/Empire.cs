aadc8feb566a896bc03e83924951fb742a119c88
Aaron <presin16@gmail.com>
2019-10-07 22:53:08 -0500

    Moved code from Program to Empire class.

--- Stellar Empire Grinder/Empire.cs
+++ Stellar Empire Grinder/Empire.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Stellar_Empire_Grinder
@@ -22,8 +23,15 @@ namespace Stellar_Empire_Grinder
         public int ScienceDeciles { get; set; }
         public int XenoDiplomacyDeciles { get; set; }
         public int FleetDeciles { get; set; }
+        public int SpaceYear { get; set; }
+        public double DevelopmentBucket { get; set; }
+        public double ExplorationBucket { get; set; }
+        public double ExpansionBucket { get; set; }
+        public double ScienceBucket { get; set; }
+        public double FleetBucket { get; set; }
 
-        public Empire(string name, string adjective, int economy, int fleet, int colonizedPlanets, int habitablePlanets, int economyDecilesRemaining, int generalWelfareDeciles, int explorationDeciles, int expansionDeciles, int developmentDeciles, int scienceDeciles, int xenoDiplomacyDeciles, int fleetDeciles)
+        internal Empire(string name, string adjective, int economy, int fleet, int colonizedPlanets, int habitablePlanets, int economyDecilesRemaining, int generalWelfareDeciles, int explorationDeciles, 
+            int expansionDeciles, int developmentDeciles, int scienceDeciles, int xenoDiplomacyDeciles, int fleetDeciles, int spaceYear, int developmentBucket, int explorationBucket, int expansionBucket, int scienceBucket, int fleetBucket)
         {
             this.Name = name;
             this.Adjective = adjective;
@@ -39,17 +47,171 @@ namespace Stellar_Empire_Grinder
             this.ScienceDeciles = scienceDeciles;
             this.XenoDiplomacyDeciles = xenoDiplomacyDeciles;
             this.FleetDeciles = fleetDeciles;
+            this.SpaceYear = spaceYear;
+            this.DevelopmentBucket = developmentBucket;
+            this.ExplorationBucket = explorationBucket;
+            this.ExpansionBucket = expansionBucket;
+            this.ScienceBucket = scienceBucket;
+            this.FleetBucket = fleetBucket;
         }
 
-        public void EmpireInitialization()
+        internal void DecileDistribution()
         {
-            Console.WriteLine("What is the name of your empire? i.e., America");
-            string empireName = Console.ReadLine();
-            Console.WriteLine("What is the adjective for your empire? i.e., American");
-            string empireAdjective = Console.ReadLine();
-            Console.WriteLine("With the discovery of the a new habitable world, the " + empireAdjective + " people enter a new era.");
-            Console.WriteLine("This becomes known as \"Space Year One\" as humanity unites to colonize the stars.");
-            Empire player1 = new Empire(empireName, empireAdjective, 10, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0);
+            Console.WriteLine("Let's determine the spending priorities of your galactic empire!");
+            Console.WriteLine("To do this, we will assign 10 \"tenths\" of your economy to different categories.");
+            Console.WriteLine("There are " + EconomyDecilesRemaining + " tenths remaining.");
+
+            while (EconomyDecilesRemaining > 0)
+            {
+                Console.WriteLine("Where do you want to put the next tenth of your economic priorities? There are " + EconomyDecilesRemaining + " left.");
+                Console.WriteLine("Currently, there are:");
+                Console.WriteLine("1) " + GeneralWelfareDeciles + " tenths devoted to general welfare.");
+                Console.WriteLine("2) " + DevelopmentDeciles + " tenths devoted to economic development.");
+                Console.WriteLine("3) " + ExplorationDeciles + " tenths devoted to galactic exploration.");
+                Console.WriteLine("4) " + ExpansionDeciles + " tenths devoted to empire expansion.");
+                Console.WriteLine("5) " + ScienceDeciles + " tenths devoted to scientific research.");
+                Console.WriteLine("6) " + FleetDeciles + " tenths devoted to fleet production.");
+                Console.WriteLine("7) " + XenoDiplomacyDeciles + " tenths devoted to xeno diplomacy.");
+                Console.WriteLine("Press a number, 1-7, to add a tenth of your economy to a priority.");
+                Console.WriteLine("Press \"H\" for a description of the categories.");
+                char allocate = Convert.ToChar(Console.ReadLine());
+                Console.WriteLine("");
+                // Case statement 1-6 to assign deciles and subtract 1 from deciles remaining.
+                switch (allocate)
+                {
+                    case '1':
+                        Console.WriteLine("One-tenth to general welfare.");
+                        GeneralWelfareDeciles += 1;
+                        EconomyDecilesRemaining -= 1;
+                        Console.WriteLine(EconomyDecilesRemaining);
+                        break;
+                    case '2':
+                        Console.WriteLine("One-tenth to economic development.");
+                        DevelopmentDeciles += 1;
+                        EconomyDecilesRemaining -= 1;
+                        Console.WriteLine(EconomyDecilesRemaining);
+                        break;
+                    case '3':
+                        Console.WriteLine("One-tenth to galactic exploration.");
+                        ExplorationDeciles += 1;
+                        EconomyDecilesRemaining -= 1;
+                        Console.WriteLine(EconomyDecilesRemaining);
+                        break;
+                    case '4':
+                        Console.WriteLine("One-tenth to empire expansion.");
+                        ExpansionDeciles += 1;
+                        EconomyDecilesRemaining -= 1;
+                        Console.WriteLine(EconomyDecilesRemaining);
+                        break;
+                    case '5':
+                        Console.WriteLine("One-tenth to scientific research.");
+                        ScienceDeciles += 1;
+                        EconomyDecilesRemaining -= 1;
+                        Console.WriteLine(EconomyDecilesRemaining);
+                        break;
+                    case '6':
+                        Console.WriteLine("One-tenth to fleet production.");
+                        FleetDeciles += 1;
+                        EconomyDecilesRemaining -= 1;
+                        Console.WriteLine(EconomyDecilesRemaining);
+                        break;
+                    case '7':
+                        Console.WriteLine("One-tenth to xeno diplomacy.");
+                        XenoDiplomacyDeciles += 1;
+                        EconomyDecilesRemaining -= 1;
+                        Console.WriteLine(EconomyDecilesRemaining);
+                        break;
+                    case 'H':
+                        Console.WriteLine("General welfare increases your citizen's well-being. This lowers the risk of adverse events like rebellion and the rate of piracy. (This is not implemented yet.)");
+                        Console.WriteLine("Economic development increases the infrastructure on your planets, resulting in a larger \"pool\" of resources each turn.");
+                        Console.WriteLine("Galactic exploration is how you discover alien races and colonizable planets.");
+                        Console.WriteLine("Empire expansion involves the construction, manning, and fueling of colony ships. When a ship is done, a planet will be colonized, furthering economic growth.");
+                        Console.WriteLine("Scientific research results in discoveries, each of which increases your output in one of the categories going forward. (This is not implemented yet.)");
+                        Console.WriteLine("Fleet production is shipbuilding. When a ship is built, it joins your fleet. In a war, the empire with the stronger fleet annhilates the empire with the weaker fleet over a period of years, and adds the loser's planets to their colonizable planets.");
+                        Console.WriteLine("Xeno diplomacy involves the study of discovered empires, and the training and staffing of diplomats to maintain peaceful relations with those empires.");
+                        Console.WriteLine("Press any key to return to resource allocation.");
+                        Console.ReadLine();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input. Please enter a number from 1 to 6.");
+                        break;
+
+                }
+            }
+        }
+
+
+        internal void Turn()
+        {
+            
+            // int xenoDiplomacyBucket = 0;
+            // int generalWelfareBucket = 0;
+
+            
+            Console.WriteLine("Space Year " + SpaceYear);
+            Console.WriteLine("Your empire spans " + ColonizedPlanets + " colonized worlds.");
+            Console.WriteLine("Your empire's GDP is " + Economy + " gigacredits.");
+            Console.WriteLine("Your fleet is rated at " + Fleet + " strength.");
+            Console.WriteLine("Your astronomers are aware of " + HabitablePlanets + " worlds.");
+            Console.WriteLine(DevelopmentBucket);
+            Thread.Sleep(1500);
+            SpaceYear += 1;
+            //Console.WriteLine("DevelopmentDeciles = " + DevelopmentDeciles);
+            //Console.WriteLine("Math = " + (DevelopmentDeciles * Economy));
+            //Console.WriteLine("Division = " + ((DevelopmentDeciles * Economy) / 10));
+            DevelopmentBucket += ((DevelopmentDeciles * Economy) / 10);
+            Console.WriteLine(DevelopmentBucket);
+            if (DevelopmentBucket >= 15)
+            {
+                DevelopmentBucket -= 15;
+                Economy += 1;
+                Console.WriteLine("Your GDP has grown!");
+            }
+
+            ExplorationBucket += ((ExplorationDeciles * Economy) / 10);
+            if (ExplorationBucket >= 15)
+            {
+                ExplorationBucket -= 15;
+                // Random 1-4 for planet roll
+                HabitablePlanets += 1;
+                Console.WriteLine("Your scouts have discovered a colonizable planet!");
+            }
+            ExpansionBucket += ((ExpansionDeciles * Economy) / 10);
+            if (ExpansionBucket >= 15)
+            {
+                ExpansionBucket -= 15;
+                
+                ColonizedPlanets += 1;
+                HabitablePlanets -= 1;
+                Console.WriteLine("Your people have settled a new world!");
+            }
+            // ScienceBucket += ((ScienceDeciles * Economy) / 10);
+            FleetBucket += ((FleetDeciles * Economy) / 10);
+            if (FleetBucket >= 15)
+            {
+                FleetBucket -= 15;
+                Fleet += 1;
+                Console.WriteLine("Your navy commissions a new ship!");
+            }
+            // welfare ExplorationBucket += ((ExplorationDeciles * Economy) / 10);
+            // diplomacy ExplorationBucket += ((ExplorationDeciles * Economy) / 10);
+
+            GamePlay();
+        }
+        internal void GamePlay()
+        {
+            {
+                Console.WriteLine("Press \"A\" to adjust resource allocation.");
+                if (!Console.KeyAvailable)
+                {
+                    Turn();
+                    
+                }
+                else
+                {
+                    DecileDistribution();
+                }
+            }
         }
     }
 }
--- Stellar Empire Grinder/Program.cs
+++ Stellar Empire Grinder/Program.cs
@@ -18,147 +18,28 @@ namespace Stellar_Empire_Grinder
             Console.WriteLine("Thanks!");
             */
 
-            EmpireInitialization();
+            Console.WriteLine("What is the name of your empire? i.e., America");
+            string empireName = Console.ReadLine();
+            Console.WriteLine("What is the adjective for your empire? i.e., American");
+            string empireAdjective = Console.ReadLine();
+            Console.WriteLine("With the discovery of the a new habitable world, the " + empireAdjective + " people enter a new era.");
+            Console.WriteLine("This becomes known as \"Space Year One\" as humanity unites to colonize the stars.");
 
-            DecileDistribution();
+            Empire player1 = new Empire(empireName, empireAdjective, 10, 1, 1, 1, 10, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0);
 
-            GamePlay();
+            player1.DecileDistribution();
 
-            
-            
 
-            }
-           static void DecileDistribution()
-        {
-            Console.WriteLine("Let's determine the spending priorities of your galactic empire!");
-            Console.WriteLine("To do this, we will assign 10 \"tenths\" of your economy to different categories.");
 
-            int economyDecilesRemaining = 10;
-            int generalWelfareDeciles = 0;
-            int explorationDeciles = 0;
-            int expansionDeciles = 0;
-            int developmentDeciles = 0;
-            int scienceDeciles = 0;
-            int xenoDiplomacyDeciles = 0;
-            int fleetDeciles = 0;
+            //DecileDistribution();
 
-            Console.WriteLine("There are " + economyDecilesRemaining + " tenths remaining.");
 
-            while (economyDecilesRemaining > 0)
-            {
-                Console.WriteLine("Where do you want to put the next tenth of your economic priorities? There are " + economyDecilesRemaining + " left.");
-                Console.WriteLine("Currently, there are:");
-                Console.WriteLine("1) " + generalWelfareDeciles + " tenths devoted to general welfare.");
-                Console.WriteLine("2) " + developmentDeciles + " tenths devoted to economic development.");
-                Console.WriteLine("3) " + explorationDeciles + " tenths devoted to galactic exploration.");
-                Console.WriteLine("4) " + expansionDeciles + " tenths devoted to empire expansion.");
-                Console.WriteLine("5) " + scienceDeciles + " tenths devoted to scientific research.");
-                Console.WriteLine("6) " + fleetDeciles + " tenths devoted to fleet production.");
-                Console.WriteLine("7 " + xenoDiplomacyDeciles + " tenths devoted to xeno diplomacy.");
-                Console.WriteLine("Press a number, 1-7, to add a tenth of your economy to a priority.");
-                Console.WriteLine("Press \"H\" for a description of the categories.");
-                char allocate = Convert.ToChar(Console.ReadLine());
-                Console.WriteLine("");
-                // Case statement 1-6 to assign deciles and subtract 1 from deciles remaining.
-                switch (allocate)
-                {
-                    case '1':
-                        Console.WriteLine("One-tenth to general welfare.");
-                        generalWelfareDeciles += 1;
-                        economyDecilesRemaining -= 1;
-                        Console.WriteLine(economyDecilesRemaining);
-                        break;
-                    case '2':
-                        Console.WriteLine("One-tenth to economic development.");
-                        developmentDeciles += 1;
-                        economyDecilesRemaining -= 1;
-                        Console.WriteLine(economyDecilesRemaining);
-                        break;
-                    case '3':
-                        Console.WriteLine("One-tenth to galactic exploration.");
-                        explorationDeciles += 1;
-                        economyDecilesRemaining -= 1;
-                        Console.WriteLine(economyDecilesRemaining);
-                        break;
-                    case '4':
-                        Console.WriteLine("One-tenth to empire expansion.");
-                        expansionDeciles += 1;
-                        economyDecilesRemaining -= 1;
-                        Console.WriteLine(economyDecilesRemaining);
-                        break;
-                    case '5':
-                        Console.WriteLine("One-tenth to scientific research.");
-                        scienceDeciles += 1;
-                        economyDecilesRemaining -= 1;
-                        Console.WriteLine(economyDecilesRemaining);
-                        break;
-                    case '6':
-                        Console.WriteLine("One-tenth to fleet production.");
-                        fleetDeciles += 1;
-                        economyDecilesRemaining -= 1;
-                        Console.WriteLine(economyDecilesRemaining);
-                        break;
-                    case '7':
-                        Console.WriteLine("One-tenth to xeno diplomacy.");
-                        xenoDiplomacyDeciles += 1;
-                        economyDecilesRemaining -= 1;
-                        Console.WriteLine(economyDecilesRemaining);
-                        break;
-                    case 'H':
-                        Console.WriteLine("General welfare increases your citizen's well-being. This lowers the risk of adverse events like rebellion and the rate of piracy. (This is not implemented yet.)");
-                        Console.WriteLine("Economic development increases the infrastructure on your planets, resulting in a larger \"pool\" of resources each turn.");
-                        Console.WriteLine("Galactic exploration is how you discover alien races and colonizable planets.");
-                        Console.WriteLine("Empire expansion involves the construction, manning, and fueling of colony ships. When a ship is done, a planet will be colonized, furthering economic growth.");
-                        Console.WriteLine("Scientific research results in discoveries, each of which increases your output in one of the categories going forward. (This is not implemented yet.)");
-                        Console.WriteLine("Fleet production is shipbuilding. When a ship is built, it joins your fleet. In a war, the empire with the stronger fleet annhilates the empire with the weaker fleet over a period of years, and adds the loser's planets to their colonizable planets.");
-                        Console.WriteLine("Xeno diplomacy involves the study of discovered empires, and the training and staffing of diplomats to maintain peaceful relations with those empires.");
-                        Console.WriteLine("Press any key to return to resource allocation.");
-                        Console.ReadLine();
-                        break;
-                    default:
-                        Console.WriteLine("Invalid input. Please enter a number from 1 to 6.");
-                        break;
-                        
-                }
-            }
+            player1.Turn();
+
+            player1.GamePlay();
+
 
         }
-           static void Turn()
-        {
-            
-            int developmentBucket = 0;
-            int explorationBucket = 0;
-            int expansionBucket = 0;
-            int scienceBucket = 0;
-            int fleetBucket = 0;
-            // int xenoDiplomacyBucket = 0;
-            // int generalWelfareBucket = 0;
-
-            //Console.WriteLine("Space Year " + spaceYear);
-            Console.WriteLine("Your empire spans " + player1.colonizedPlanets + " colonized worlds.");
-            Console.WriteLine("Your empire's GDP is " + playerEconomy + " gigacredits.");
-            Console.WriteLine("Your fleet is rated at " + playerFleet + " strength.");
-            Console.WriteLine("Your astronomers are aware of " + playerHabitablePlanets + " worlds.");
-            Thread.Sleep(300);
-
-            GamePlay();
-        }
-           static void GamePlay()
-            {
-                //int spaceYear = 1;
-                Console.WriteLine("Press \"A\" to adjust resource allocation.");
-                if(!Console.KeyAvailable)
-                {
-                    Turn();
-                //spaceYear += 1;
-                }
-                else
-                {
-                    DecileDistribution();
-                }
-            }
-
-           
     }
 }
 
@@ -167,7 +48,7 @@ namespace Stellar_Empire_Grinder
  General Welfare
     Rebellion - subtracts from fleet (percentage?)
  Piracy - subtracts from economy (percentage?) - int piracy - 
- SpaceYear
+ // Random 1-4 for planet roll
  ScientificOutputBonus for each category
  Re-check help descriptions
  How do planets affect economic growth? Increase cost of development by devBucket, increase returns by planetMultiplier? - should scale at greater than 1:1
